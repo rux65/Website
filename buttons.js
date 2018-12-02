bbc255c4deda3c736a1ec33ae8fd26c01c1e6eb2
Ruxi <ruxi_65@yahoo.com>
2018-12-02 01:22:39 +0000

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -9,6 +9,10 @@ function myFunction2() {
     document.getElementById("myDropdown2").classList.toggle("show");
 }
 
+function myFunction3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
+}
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
